3afccf6446ede4a5d854a41cde51bc0af16fb421
jungjinbeom <insunok0715@naver.com>
2023-04-02 16:21:52 +0900

    first commit

---
+++ app.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+app.get("/products", (req, res) => {
+  const products = [
+    {
+      category: "Fruits",
+      price: "$1",
+      stocked: true,
+      name: "Apple",
+    },
+    {
+      category: "Fruits",
+      price: "$1",
+      stocked: true,
+      name: "Dragonfruit",
+    },
+    {
+      category: "Fruits",
+      price: "$2",
+      stocked: false,
+      name: "Passionfruit",
+    },
+    {
+      category: "Vegetables",
+      price: "$2",
+      stocked: true,
+      name: "Spinach",
+    },
+    {
+      category: "Vegetables",
+      price: "$4",
+      stocked: false,
+      name: "Pumpkin",
+    },
+    {
+      category: "Vegetables",
+      price: "$1",
+      stocked: true,
+      name: "Peas",
+    },
+  ];
+
+  res.send({ products });
+});
